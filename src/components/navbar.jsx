c797e3a806c7498fc943528130bb67203c38b3ad
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-06 17:42:13 +0330

    Adding right icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
-class Navbar extends Component {
+class Navbar extends React.Component {
     state = {};
 
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
-                    <a href="" className="navbar-brand text-secondary">React Todo App</a>
+                    <a href="." className="navbar-brand text-secondary">React Todo App</a>
+                    <div className="d-flex align-items-center">
+                        <a className="text-reset me-3" href="#"><i className=""><FontAwesomeIcon icon={faBars} /></i></a>
+                    </div>
                 </div>
             </nav>
         )
