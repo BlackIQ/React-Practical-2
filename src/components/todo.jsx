b317793afdd0aee24bc31c4da91cce22d3383917
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-06 17:27:55 +0330

    Remove sction dropdown stuff

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCheck, faTrash, faClock, faBars} from "@fortawesome/free-solid-svg-icons";
+import {faCheck, faTrash, faClock, faCaretDown} from "@fortawesome/free-solid-svg-icons";
 
 class Todo extends Component {
     render() {
@@ -14,10 +14,29 @@ class Todo extends Component {
                     </span>
                     <div className="actions">
                         <div className="dropdown">
-                            <button className={this.actionClass()} type="button" id="dropmenu" data-mdb-toggle="dropdown" aria-expanded="false">
-                                <FontAwesomeIcon icon={faBars} />
-                            </button>
-                            {this.actions()}
+                            <span className="text-dark pointer" id="dropmenu" data-mdb-toggle="dropdown" aria-expanded="false">
+                                <FontAwesomeIcon icon={faCaretDown} />
+                            </span>
+                            <ul className="dropdown-menu" aria-labelledby="dropmenu">
+                                <li>
+                                    <button onClick={() => this.props.onDone(this.props.todo)} className="dropdown-item text-success" type="button" disabled={this.props.todo.status === 'done' ? true : null}>
+                                        Done
+                                        <span className="float-end"><FontAwesomeIcon icon={faCheck} /></span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => this.props.onNotDone(this.props.todo)} className="dropdown-item text-primary" type="button" disabled={this.props.todo.status === 'not' ? true : null}>
+                                        Not Done
+                                        <span className="float-end"><FontAwesomeIcon icon={faClock} /></span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => this.props.onDelete(this.props.todo)} className="dropdown-item text-danger" type="button" disabled={this.props.todo.status === 'trash' ? true : null}>
+                                        Delete
+                                        <span className="float-end"><FontAwesomeIcon icon={faTrash} /></span>
+                                    </button>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -25,63 +44,8 @@ class Todo extends Component {
         )
     }
 
-    actions() {
-        if (this.props.todo.status === 'not') {
-            return (
-                <ul className="dropdown-menu" aria-labelledby="dropmenu">
-                    <li>
-                        <button onClick={() => this.props.onDone(this.props.todo)} className="dropdown-item text-success" type="button">
-                            Done
-                            <span className="float-end"><FontAwesomeIcon icon={faCheck} /></span>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => this.props.onDelete(this.props.todo)} className="dropdown-item text-danger" type="button">
-                            Delete
-                            <span className="float-end"><FontAwesomeIcon icon={faTrash} /></span>
-                        </button>
-                    </li>
-                </ul>
-            );
-        } else if (this.props.todo.status === 'done') {
-            return (
-                <ul className="dropdown-menu" aria-labelledby="dropmenu">
-                    <li>
-                        <button onClick={() => this.props.onNotDone(this.props.todo)} className="dropdown-item text-primary" type="button">
-                            Not Done
-                            <span className="float-end"><FontAwesomeIcon icon={faClock} /></span>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => this.props.onDelete(this.props.todo)} className="dropdown-item text-danger" type="button">
-                            Delete
-                            <span className="float-end"><FontAwesomeIcon icon={faTrash} /></span>
-                        </button>
-                    </li>
-                </ul>
-            );
-        } else if (this.props.todo.status === 'trash') {
-            return (
-                <ul className="dropdown-menu" aria-labelledby="dropmenu">
-                    <li>
-                        <button onClick={() => this.props.onDone(this.props.todo)} className="dropdown-item text-success" type="button">
-                            Done
-                            <span className="float-end"><FontAwesomeIcon icon={faCheck} /></span>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={() => this.props.onNotDone(this.props.todo)} className="dropdown-item text-primary" type="button">
-                            Not Done
-                            <span className="float-end"><FontAwesomeIcon icon={faClock} /></span>
-                        </button>
-                    </li>
-                </ul>
-            );
-        }
-    }
-
     actionClass() {
-        let c = 'btn btn-';
+        let c = 'text-';
         if (this.props.todo.status === 'not') c += 'primary';
         else if (this.props.todo.status === 'done') c += 'success';
         else if (this.props.todo.status === 'trash') c += 'danger';
