f1a9726352bf77b71782a47f22dc907e6991181c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-06 18:45:07 +0330

    Done in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {FaGithub, FaGlobeAmericas} from "react-icons/fa";
+import {FaGithub, FaGlobeAmericas, FaPlus} from "react-icons/fa";
+import Inputs from "./inputs";
 
 class Navbar extends React.Component {
     state = {};
@@ -10,8 +11,9 @@ class Navbar extends React.Component {
                 <div className="container-fluid">
                     <a href="." className="navbar-brand text-secondary">React Todo App</a>
                     <div className="d-flex align-items-center">
-                        <a className="text-reset me-3" href="https://amirhossein.info"><i className=""><FaGlobeAmericas /></i></a>
-                        <a className="text-reset me-3" href="https://github.com/BlackIQ/React-Practical-2"><i className=""><FaGithub /></i></a>
+                        <Inputs />
+                        <a className="text-reset me-3" href="https://amirhossein.info"><FaGlobeAmericas /></a>
+                        <a className="text-reset me-3" href="https://github.com/BlackIQ/React-Practical-2"><FaGithub /></a>
                     </div>
                 </div>
             </nav>
