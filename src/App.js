022c02676bba63c1b3b54c55e8fe6496de75df44
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-06 10:55:56 +0330

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Todo from "./components/todo";
+import Navbar from "./components/navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <Navbar />
+            <main className="container body">
+                <div className="row">
+                    <Todo/>
+                    <Todo/>
+                    <Todo/>
+                    <Todo/>
+                    <Todo/>
+                    <Todo/>
+                </div>
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
